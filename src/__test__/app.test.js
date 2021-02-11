cdd8005d56e1ba0c245fe5e917ebcd297eb24659
Benjamin Langley <blangley@mitre.org>
2021-02-11 13:28:55 -0500

    Fix tests to use new endpoints

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -5,28 +5,28 @@ const bundle = {};
 describe('Test the root path', () => {
   test('It should echo on anonymize POST', () => {
     return request(app)
-      .post('/anonymize')
+      .post('/Bundle/$anonymize')
       .send(bundle)
       .expect(200, bundle);
   });
 
   test('It should echo on pseudonymize POST', () => {
     return request(app)
-      .post('/pseudonymize')
+      .post('/Bundle/$pseudonymize')
       .send(bundle)
       .expect(200, bundle);
   });
 
   test('It should echo on reidentify POST', () => {
     return request(app)
-      .post('/reidentify')
+      .post('/Bundle/$reidentify')
       .send(bundle)
       .expect(200, bundle);
   });
 
   test('It should echo on deidentify POST', () => {
     return request(app)
-      .post('/deidentify')
+      .post('/Bundle/$deidentify')
       .send(bundle)
       .expect(200, bundle);
   });
